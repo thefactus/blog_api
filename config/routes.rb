62f8a26c54826fc7ca8499b9a567011407dce3af
Pablo Bello <thefactus@gmail.com>
2016-09-11 17:48:40 -0300

    Set api constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    end
+  end
 end
